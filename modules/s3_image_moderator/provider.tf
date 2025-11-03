4c1c3d058d6bed426bd8bb7f8eb890a43e88b45a
Liantsoa Rasata <rmliantsoa@gmail.com>
2025-11-03 15:58:15 +0100

    chore: upgrade aws provider to 6.18 and terraform min 1.6

--- modules/s3_image_moderator/provider.tf
+++ modules/s3_image_moderator/provider.tf
@@ -1,12 +1,13 @@
 terraform {
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 6.4"
+      version = "~> 6.18"
     }
   }
 
-  required_version = ">= 1.3"
+  required_version = ">= 1.6.0, < 2.0.0"
 }
 
 provider "aws" {
