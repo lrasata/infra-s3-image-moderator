1088a15b6865e5967da4dc854a24440aff0fcdd7
Liantsoa Rasata <rmliantsoa@gmail.com>
2026-01-06 16:11:10 +0100

    fix: topics should be encrypted

--- modules/s3_image_moderator/sns.tf
+++ modules/s3_image_moderator/sns.tf
@@ -1,5 +1,22 @@
+resource "aws_kms_key" "sns_cmk" {
+  description         = "SNS CMK"
+  enable_key_rotation = true
+  tags = {
+    Environment = var.environment
+    App         = var.app_id
+  }
+}
+
 resource "aws_sns_topic" "scan_alerts" {
   name = "${var.environment}-inappropriate-image-alerts"
+
+  kms_master_key_id = aws_kms_key.sns_cmk.arn
+
+  tags = {
+    Service     = "image-moderation"
+    Environment = var.environment
+    App         = var.app_id
+  }
 }
 
 resource "aws_sns_topic_subscription" "admin_email" {
